a8821e32c8b8fcb51f8e85a249ffc300561c9742
usk0930 <usk0930@outlook.jp>
2023-04-03 16:13:24 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/signup', to: "users#new", as: 'signup'
+
   root "static_pages#home"
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/about'
+  # rootが設定されていれば、パスは自動で
+  get  "/help",    to: "static_pages#help" # 例：help_path ⇨helpメソッドが複数あったらどう区別する？
+  get  "/about",   to: "static_pages#about", as: 'ab'
+  get  "/contact", to: "static_pages#contact"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -10,6 +10,9 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # includeはモジュールを呼んでいる
+  # TODO:requireとの違いは？
+  include ApplicationHelper
 
   # Add more helper methods to be used by all tests here...
 end
